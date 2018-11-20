7481e04c3b5c4f45428bd1d7b589e9eb5f599873
Leigh Dennis <ldenn@umich.edu>
2018-11-19 21:37:06 -0500

    this is my first commit

---
+++ cookiecode.py
@@ -0,0 +1,16 @@
+#This is python practice exercise
+
+print "Please enter your choice of cookie: "
+print "a) Chocolate Chip"
+print "b) Oatmeal"
+print "c) Snickerdoodle"
+cookie_choice = raw_input()
+if cookie_choice == 'a':
+     print "I like chocolate chip cookies too!"
+elif cookie_choice == 'b':
+    print "Oatmeal cookies are delicious!"
+elif cookie_choice == 'c':
+    print "Ohhhh Snickerdoodles are a classic!"
+
+print "------------------------------"
+print "THANKS FOR PLAYING"
